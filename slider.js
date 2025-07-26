59f0cbdfe548f5680068bb819a4c5332caa6024f
Anja Leskovsek <anja.leskovsek@gmail.com>
2025-07-26 23:10:33 +0200

    CircularSlider class created

--- slider.js
+++ slider.js
@@ -1,1 +1,14 @@
 
+class CircularSlider {
+  constructor(options) {
+    this.container = options.container;
+    this.color = options.color || "#000";
+    this.min = options.min || 0;
+    this.max = options.max || 100;
+    this.step = options.step || 1;
+    this.radius = options.radius || 100;
+    this.value = this.min;
+    this.angle = -Math.PI / 2;
+    this.drawSlider();
+  }
+}
